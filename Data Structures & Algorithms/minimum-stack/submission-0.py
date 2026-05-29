fa5baca3a86ede9e25ba2f3ed6cc4dacab84f06f
Yash Mittal <mymittal777@gmail.com>
2026-05-29 22:54:16 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,25 @@
+class MinStack:
+
+    def __init__(self):
+        self.a = []
+        
+
+    def push(self, val: int) -> None:
+        if len(self.a)<1:
+            self.a.append([val, val])
+            return
+        self.a.append([val, min(self.a[-1][1], val)])
+        
+
+    def pop(self) -> None:
+        if self.a:
+            self.a.pop()
+        
+
+    def top(self) -> int:
+        return self.a[-1][0]
+        
+
+    def getMin(self) -> int:
+        return self.a[-1][1]
+        
