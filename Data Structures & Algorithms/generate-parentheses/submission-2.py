fcc9c8dc04525797ea78c1634cad3396cbb8bcbe
Yash Mittal <mymittal777@gmail.com>
2026-07-10 08:29:08 +0530

    Add: generate-parentheses - submission-2

---
+++ Data Structures & Algorithms/generate-parentheses/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        if n==1:
+            return ["()"]
+        p = self.generateParenthesis(n-1)
+        s=set()
+        for i in p:
+            # s.add("(" + i + ")")
+            for j in range(len(i)):
+                s.add(i[:j] + "()" + i[j:])
+        return list(s)
+        
