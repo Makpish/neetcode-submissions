f709452b6394e0a1271f252066134ce6fdbbec64
Yash Mittal <mymittal777@gmail.com>
2026-07-11 20:37:08 +0530

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rowsetmap = {i:set() for i in range(9)}
+        colsetmap = {i:set() for i in range(9)}
+        gridsetmap = {i:set() for i in range(9)}
+
+        def gridnumber(i,j):
+            return (i//3)*3 + j//3
+
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] != ".":
+                    if board[i][j] in rowsetmap[i]:
+                        return False
+                    rowsetmap[i].add(board[i][j])
+
+                    if board[i][j] in colsetmap[j]:
+                        return False
+                    colsetmap[j].add(board[i][j])
+
+                    if board[i][j] in gridsetmap[gridnumber(i,j)]:
+                        return False
+                    gridsetmap[gridnumber(i,j)].add(board[i][j])
+        return True
+
+                    
+        
